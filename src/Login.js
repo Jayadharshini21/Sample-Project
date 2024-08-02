1b7ab559f48fd21c602c9f87aa2cda4ac7e8e508
Jayadharshini21 <jayadharshini.karthikeyabalaji@contractio>
2024-08-02 15:54:14 +0530

    Recent changes

---
+++ src/Login.js
@@ -0,0 +1,71 @@
+// src/components/LoginPage.js
+
+import React from 'react';
+import { Form, Input, Button, Alert } from 'antd';
+import './Login.css';
+import 'antd/dist/antd.css'; // Ant Design styles
+
+const Login = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="container">
+        <div className="login-form">
+        <Form
+          form={form}
+          name="login"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          layout="vertical"
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: 'Please enter your name' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: 'Please enter your email' },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: 'Please enter your password' },
+              { min: 8, message: 'Password must be at least 8 characters' }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+         
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
